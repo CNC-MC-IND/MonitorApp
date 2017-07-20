95320dd1df6eb21c53d1dae18fcc0876bde85d83
jumpsnack <kir0302@gmail.com>
2017-07-20 12:00:58 +0900

    [init] first commit

---
+++ app/src/main/java/kr/ac/kmu/ncs/cnc_mc_monitor/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kmu.ncs.cnc_mc_monitor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
